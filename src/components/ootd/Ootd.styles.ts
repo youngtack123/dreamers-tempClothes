09987f7f5c78d16d060bbc8fbcebbe4635878cdc
aaasoly <imnamesol@gmail.com>
2022-05-16 14:34:26 +0900

    Create ootd

---
+++ src/components/ootd/Ootd.styles.ts
@@ -0,0 +1,211 @@
+import styled from "@emotion/styled";
+
+export const Container__Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Container_head__Div = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const Container_Body__Div = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+`;
+
+export const TodayInfo__Div = styled.div`
+  width: 200px;
+  height: 80px;
+  background: #ffffff;
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+  padding: 24px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 26px;
+  margin-right: 20px;
+`;
+
+export const Date__Div = styled.div`
+  width: 66px;
+  height: 32px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Month__Span = styled.span`
+  font-weight: 600;
+  font-size: 26px;
+  color: rgba(51, 51, 51, 0.7);
+`;
+
+export const Day__Span = styled.span`
+  font-weight: 600;
+  font-size: 26px;
+`;
+
+export const Temp__Span = styled.span`
+  font-weight: 600;
+  font-size: 26px;
+  color: #b7e576;
+`;
+
+export const TagSeleted__Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 15px;
+  height: 80px;
+`;
+
+export const TagSeletedBack__Span = styled.span`
+  background: #fff2b2;
+  border-radius: 50px;
+  line-height: 32px;
+  width: 71px;
+  height: 32px;
+  text-align: center;
+`;
+
+export const TagWrapper__Div = styled.div`
+  width: 203px;
+  height: 730px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  /* padding: 13px 14px 15px 83px; */
+  border: 1px solid #bebebe;
+  border-radius: 10px;
+  position: fixed;
+  background-color: #fff;
+`;
+
+export const TagWrapper_Head__Div = styled.div`
+  width: 203px;
+  height: 38px;
+  padding-bottom: 12px;
+  border-bottom: 1px solid #bebebe;
+`;
+
+export const Tag__Span = styled.span`
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const TagBox__Div = styled.div`
+  width: 175px;
+  height: 97px;
+  border-bottom: 1px solid #bebebe;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 9px 14px 16px 15px;
+`;
+
+export const TagCategory__Span = styled.span`
+  font-weight: 600;
+  font-size: 14px;
+  margin-bottom: 15px;
+`;
+
+export const RegionTagsBox__Div = styled.div`
+  width: 146px;
+  height: 44px;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin-left: 3px;
+`;
+
+export const RegionTags__Span = styled.span`
+  font-size: 12px;
+  margin-right: 8px;
+  margin-bottom: 3px;
+`;
+
+export const StyleTagsBox__Div = styled.div`
+  width: 146px;
+  height: 44px;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin-left: 3px;
+`;
+
+export const StyleTags__Span = styled.span`
+  font-size: 12px;
+  margin-right: 8px;
+  margin-bottom: 3px;
+`;
+
+export const OuterTagsBox__Div = styled.div`
+  width: 146px;
+  height: 44px;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin-left: 3px;
+`;
+
+export const OuterTags__Span = styled.span`
+  font-size: 12px;
+  margin-right: 8px;
+  margin-bottom: 3px;
+`;
+
+export const TopTagsBox__Div = styled.div`
+  width: 146px;
+  height: 44px;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin-left: 3px;
+`;
+
+export const TopTags__Span = styled.span`
+  font-size: 12px;
+  margin-right: 8px;
+  margin-bottom: 3px;
+`;
+
+export const BottomTagsBox__Div = styled.div`
+  width: 146px;
+  height: 44px;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin-left: 3px;
+`;
+
+export const BottomTags__Span = styled.span`
+  font-size: 12px;
+  margin-right: 8px;
+  margin-bottom: 3px;
+`;
+
+export const EtcTagsBox__Div = styled.div`
+  width: 146px;
+  height: 44px;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin-left: 3px;
+`;
+
+export const EtcTags__Span = styled.span`
+  font-size: 12px;
+  margin-right: 8px;
+  margin-bottom: 3px;
+`;
+
+export const Feeds__Div = styled.div`
+  display: flex;
+  width: 965px;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
