d1923f59f5e7e562eec69d04d8390adbfb8dc346
Youngtack Hong <youngtack123@gmail.com>
2022-05-26 21:37:51 +0900

    :art: 헤더 로그아웃, 마이페이지 이동 메뉴바 제작
    헤더 로그아웃, 마이페이지 이동용 메뉴바 제작(mui)

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -2,7 +2,14 @@ import React, { useRef } from "react";
 import styled from "@emotion/styled";
 import { gql, useMutation, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
-import { Menu, Dropdown, Space } from "antd";
+import Button from "@mui/material/Button";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import Stack from "@mui/material/Stack";
 
 const HeaderWrapperDiv = styled.div`
   background-color: white;
@@ -43,6 +50,7 @@ const MainLogoImg = styled.img`
 const HiUserDiv = styled.div`
   font-size: 1.2rem;
   margin-left: 9.8rem;
+  margin-right: 2.7rem;
 `;
 const UserNameSpan = styled.span`
   font-weight: 600;
@@ -51,7 +59,6 @@ const UserNameSpan = styled.span`
 const ItemImg = styled.img`
   width: 3rem;
   height: 3rem;
-  margin-left: 2.7rem;
   cursor: pointer;
 `;
 
@@ -72,6 +79,40 @@ const Header = () => {
   const [m_logout] = useMutation(LOGOUT);
   const { data } = useQuery(FETCH_USER);
   const router = useRouter();
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
 
   const logout = async () => {
     try {
@@ -108,7 +149,32 @@ const Header = () => {
         <HiUserDiv>
           기온에 맞는 옷을 입고 싶은 <UserNameSpan>{data?.fetchUser.nickname}</UserNameSpan> 님, 환영합니다!
         </HiUserDiv>
-        <ItemImg src="/images/headerUser.png" />
+        <Stack direction="row" spacing={2}>
+          <div>
+            <Button ref={anchorRef} id="composition-button" aria-controls={open ? "composition-menu" : undefined} aria-expanded={open ? "true" : undefined} aria-haspopup="true" onClick={handleToggle}>
+              <ItemImg src="/images/headerUser.png" />
+            </Button>
+            <Popper open={open} anchorEl={anchorRef.current} role={undefined} placement="bottom-start" transition disablePortal>
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  style={{
+                    transformOrigin: placement === "bottom-start" ? "left top" : "left bottom",
+                  }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                      <MenuList autoFocusItem={open} id="composition-menu" aria-labelledby="composition-button" onKeyDown={handleListKeyDown}>
+                        <MenuItem onClick={moveToMyPage}>마이페이지</MenuItem>
+                        <MenuItem onClick={logout}>로그아웃</MenuItem>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+          </div>
+        </Stack>
       </HeaderContentDiv>
     </HeaderWrapperDiv>
   );
