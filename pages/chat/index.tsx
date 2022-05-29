3f4457e25ebc810a07a498763b034562ecb43f5e
alsqjarlwkd <alsqjarlwkd@naver.com>
2022-05-29 20:40:49 +0900

    chatting

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -5,6 +5,7 @@ import styled from "@emotion/styled";
 import { jsx, keyframes } from "@emotion/react";
 import { toast } from "react-toastify";
 import useUpdateEffect from "../../src/components/common/customHook/useUpdateEffect";
+import { useCallback } from "react";
 
 const CHATLOG = gql`
   query fetchLogs($opponentNickname: String!) {
@@ -125,6 +126,7 @@ const SubmitButton = styled.button`
 const ChatInputDiv = styled.div`
   margin-left: 1rem;
   margin-right: 1rem;
+  margin-bottom: 1rem;
 `;
 
 const Chat = (props) => {
@@ -135,8 +137,6 @@ const Chat = (props) => {
   const [nickNameOk, setNickNameOk] = useState(false);
   const { closeChatModal } = props;
 
-  const messageDiv = useRef(null);
-
   const { data: fetchUser } = useQuery(FETCH_USER);
   const { data, refetch } = useQuery(CHATLOG, {
     variables: {
@@ -144,8 +144,14 @@ const Chat = (props) => {
     },
   });
   const [payChat] = useMutation(CHAT_PAYMENT);
+  const scrollRef = useRef(null);
+  const inputRef = useRef(null);
 
-  useUpdateEffect(() => {
+  useEffect(() => {
+    scrollRef.current.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+  }, [data]);
+
+  useEffect(() => {
     if (fetchUser) {
       setNickNameOk(true);
     } else if (!fetchUser) {
@@ -156,6 +162,7 @@ const Chat = (props) => {
   useEffect(() => {
     if (!confirm("채팅은 500원이 차감됩니다!")) {
       // 취소(아니오) 버튼 클릭 시 이벤트
+      closeChatModal(false);
     } else {
       // 확인(예) 버튼 클릭 시 이벤트
       if (!data) {
@@ -211,13 +218,21 @@ const Chat = (props) => {
     /* 메시지 전송 */
     const nickname = fetchUser?.fetchUser.nickname;
     socket.emit("send", room, nickname, message);
+    inputRef.current.value = "";
   }
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      msg_send();
+      refetch();
+      inputRef.current.value = "";
+    }
+  };
 
   return (
     <div>
       <h1 style={{ marginLeft: "1rem", marginTop: "1rem" }}>{`${fetchUser?.fetchUser.nickname}님 반갑습니다`}</h1>
-
-      <div id="chatList" ref={messageDiv}>
+      <div>
         {data?.fetchLogs.map((el, index) => {
           if (fetchUser?.fetchUser.nickname === el?.user.nickname) {
             return (
@@ -241,14 +256,14 @@ const Chat = (props) => {
           }
         })}
       </div>
-
       <ChatInputDiv>
         <ChatInputBtnDiv>
-          <ChatInput type="text" id="msg" onChange={messageHandler} placeholder="메세지 입력을 기다리고 있어요!" />
+          <ChatInput type="text" id="msg" onChange={messageHandler} placeholder="메세지 입력을 기다리고 있어요!" onKeyPress={handleKeyPress} ref={inputRef} />
           <SubmitButton
             onClick={() => {
               msg_send(), refetch();
             }}
+            ref={scrollRef}
           >
             제출
           </SubmitButton>
