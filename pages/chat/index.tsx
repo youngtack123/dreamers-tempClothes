c5116292fab29010e08ab1e628db1795b01d5c7a
alsqjarlwkd <alsqjarlwkd@naver.com>
2022-05-29 12:39:47 +0900

    feature-login_chat

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 import React, { useState, useEffect, useRef } from "react";
 import { gql, useLazyQuery, useQuery } from "@apollo/client";
 import useUpdateEffect from "../../src/components/common/customHook/useUpdateEffect";
+import styled from "@emotion/styled";
 
 const CHATLOG = gql`
   query fetchLogs($opponentNickname: String!) {
@@ -9,9 +10,6 @@ const CHATLOG = gql`
       id
       room
       message
-      user {
-        nickname
-      }
     }
   }
 `;
@@ -37,6 +35,47 @@ const FETCH_USER = gql`
   }
 `;
 
+const ChatFlexDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ChatReceiveDiv = styled.div`
+  background: #eeeeee;
+  border-radius: 10px;
+  height: 1.8rem;
+  padding: 2rem;
+  margin-left: 1.4rem;
+  margin-bottom: 1.5rem;
+`;
+
+const ChatInputBtnDiv = styled.div`
+  display: flex;
+`;
+
+const ChatInput = styled.input`
+  width: 41.6rem;
+  height: 4.5rem;
+  border: 1px solid #bebebe;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 0px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 0px;
+  border-right: none;
+`;
+
+const SubmitButton = styled.button`
+  width: 10.6rem;
+  height: 4.5rem;
+  border: 1px solid #bebebe;
+  background-color: white;
+  border-top-left-radius: 0px;
+  border-top-right-radius: 15px;
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 15px;
+  border-left: none;
+`;
+
 const Chat = () => {
   const [message, setMessage] = useState();
   const [nickName, setNickName] = useState("");
@@ -44,14 +83,12 @@ const Chat = () => {
   const [socket, setSocket] = useState("");
   const [nickNameOk, setNickNameOk] = useState(false);
 
-  const [receiveMessage, setReceiveMessage] = useState([]);
-
   const messageDiv = useRef(null);
 
   const { data: fetchUser } = useQuery(FETCH_USER);
   const { data, refetch } = useQuery(CHATLOG, {
     variables: {
-      opponentNickname: "범기",
+      opponentNickname: fetchUser?.fetchUser.nickname,
     },
   });
 
@@ -67,14 +104,16 @@ const Chat = () => {
     if (nickNameOk) {
       const socket = io("https://team01.leo3179.shop/chat", { transports: ["websocket"], upgrade: false });
       const nickname = fetchUser?.fetchUser.nickname;
-      const room = "1";
+      const room = prompt("방번호를 입력해 주세요");
 
-      socket.emit("message", nickname, room, message);
+      socket.emit("message", room, nickname, message);
 
       socket.on("connect", () => {
         /* 누군가 채팅침 */
+
         socket.on(room, (data) => {
           console.log("누군가가 채팅침", data);
+          refetch();
         });
         /* 누군가 입장 */
         socket.on("receive" + room, (receive) => {
@@ -95,29 +134,38 @@ const Chat = () => {
   function msg_send() {
     /* 메시지 전송 */
     console.log("전송?");
-    socket.emit("send", room, nickName, message);
+    const nickname = fetchUser?.fetchUser.nickname;
+    socket.emit("send", room, nickname, message);
+    console.log("전송때 data", data);
   }
   return (
     <div>
-      <h1>
-        재형이의 시크릿쥬쥬 <span id="room"></span>
-      </h1>
+      <h1>{`${fetchUser?.fetchUser.nickname}님의 방`}</h1>
 
       <div id="chatList" ref={messageDiv}>
-        {receiveMessage.map((el, index) => {
-          return <div key={index}>{el}</div>;
+        {data?.fetchLogs.map((el, index) => {
+          return (
+            <ChatFlexDiv key={index}>
+              <img src="../images/user.png" width="50"></img>
+              <ChatReceiveDiv>
+                <span>{`${el.message}`}</span>
+              </ChatReceiveDiv>
+            </ChatFlexDiv>
+          );
         })}
       </div>
 
       <div id="sendMessage">
-        <input type="text" id="msg" onChange={messageHandler} />
-        <button
-          onClick={() => {
-            msg_send(), refetch();
-          }}
-        >
-          제출
-        </button>
+        <ChatInputBtnDiv>
+          <ChatInput type="text" id="msg" onChange={messageHandler} placeholder="메세지 입력을 기다리고 있어요!" />
+          <SubmitButton
+            onClick={() => {
+              msg_send(), refetch();
+            }}
+          >
+            제출
+          </SubmitButton>
+        </ChatInputBtnDiv>
       </div>
     </div>
   );
