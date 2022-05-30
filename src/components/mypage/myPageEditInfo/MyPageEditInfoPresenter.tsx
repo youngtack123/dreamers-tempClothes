9d8f1c95d5dc6edd8b088226e7da947afd9bcf32
alsqjarlwkd <alsqjarlwkd@naver.com>
2022-05-30 16:33:19 +0900

    mypage 수정

--- src/components/mypage/myPageEditInfo/MyPageEditInfoPresenter.tsx
+++ src/components/mypage/myPageEditInfo/MyPageEditInfoPresenter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Timer from "../../lib/timer";
 import * as S from "./MyPageEditInfoStyles";
 import { useRecoilState } from "recoil";
@@ -6,7 +6,24 @@ import { timerState } from "../../common/store/index";
 const MyPageEditInfoPresenter = (props) => {
   const [sendAuthNumber] = useRecoilState(timerState);
   const { handleInfo, onClickEventTag, updateUserEditInfo, createPhoneAuth, confirmAuthNumber, authOk, overLapId, overLapNic } = props;
+  const [clickStyle, setClickStyle] = useState("");
+  const [clickRegionTop, setClickRegionTop] = useState("");
+  const [clickRegionBottom, setRegionBottom] = useState("");
+  const styleArr = ["캐주얼", "스트릿", "트레이닝", "포멀", "세미포멀"];
+  const regionTopArr = ["서울", "경기", "강원", "충북", "충남"];
+  const regionBottomArr = ["경북", "경남", "전북", "전남", "제주"];
 
+  const onClickTagStyle = (id) => {
+    setClickStyle(id);
+  };
+
+  const onClickRegionTop = (id) => {
+    setClickRegionTop(id);
+  };
+
+  const onClickRegionBottom = (id) => {
+    setRegionBottom(id);
+  };
   return (
     <S.RealMyPageEditInfoWrapper>
       <S.MyPageEditInfoInputDiv>
@@ -40,62 +57,98 @@ const MyPageEditInfoPresenter = (props) => {
 
         <S.StyleDiv>
           <S.MyPageEditInfoInputsP>스타일</S.MyPageEditInfoInputsP>
-
-          <S.MyPageEditTag id="style" onClick={onClickEventTag}>
-            캐주얼
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="style" onClick={onClickEventTag}>
-            스트릿
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="style" onClick={onClickEventTag}>
-            트레이닝
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="style" onClick={onClickEventTag}>
-            포멀
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="style" onClick={onClickEventTag}>
-            세미포멀
-          </S.MyPageEditTag>
+          {styleArr.map((el, index) => {
+            if (el === clickStyle) {
+              return (
+                <S.MyPageEditTag
+                  id="style"
+                  onClick={(event) => {
+                    onClickEventTag(event), onClickTagStyle(el);
+                  }}
+                  style={{ background: "#FFF2B2", border: "1px solid #FFDD87" }}
+                  key={index}
+                >
+                  {el}
+                </S.MyPageEditTag>
+              );
+            } else {
+              return (
+                <S.MyPageEditTag
+                  id="style"
+                  onClick={(event) => {
+                    onClickEventTag(event), onClickTagStyle(el);
+                  }}
+                  key={index}
+                >
+                  {el}
+                </S.MyPageEditTag>
+              );
+            }
+          })}
         </S.StyleDiv>
 
         <S.RegionTopDiv>
           <S.MyPageEditInfoInputsP>거주지역</S.MyPageEditInfoInputsP>
-
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            서울
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            경기
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            강원
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            충북
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            충남
-          </S.MyPageEditTag>
+          {regionTopArr.map((el, index) => {
+            if (el === clickRegionTop) {
+              return (
+                <S.MyPageEditTag
+                  id="region"
+                  onClick={(event) => {
+                    onClickEventTag(event), onClickRegionTop(el);
+                  }}
+                  key={index}
+                  style={{ background: "#FFF2B2", border: "1px solid #FFDD87" }}
+                >
+                  {el}
+                </S.MyPageEditTag>
+              );
+            } else {
+              return (
+                <S.MyPageEditTag
+                  id="region"
+                  onClick={(event) => {
+                    onClickEventTag(event), onClickRegionTop(el);
+                  }}
+                  key={index}
+                >
+                  {el}
+                </S.MyPageEditTag>
+              );
+            }
+          })}
         </S.RegionTopDiv>
 
         <S.RegionBottomDiv>
           <S.MyPageEditInfoInputsP style={{ color: "white" }}>거주지역</S.MyPageEditInfoInputsP>
-
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            경북
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            경남
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            전북
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            전남
-          </S.MyPageEditTag>
-          <S.MyPageEditTag id="region" onClick={onClickEventTag}>
-            제주
-          </S.MyPageEditTag>
+          {regionBottomArr.map((el, index) => {
+            if (el === clickRegionBottom) {
+              return (
+                <S.MyPageEditTag
+                  id="region"
+                  onClick={(event) => {
+                    onClickEventTag(event), onClickRegionBottom(el);
+                  }}
+                  key={index}
+                  style={{ background: "#FFF2B2", border: "1px solid #FFDD87" }}
+                >
+                  {el}
+                </S.MyPageEditTag>
+              );
+            } else {
+              return (
+                <S.MyPageEditTag
+                  id="region"
+                  onClick={(event) => {
+                    onClickEventTag(event), onClickRegionBottom(el);
+                  }}
+                  key={index}
+                >
+                  {el}
+                </S.MyPageEditTag>
+              );
+            }
+          })}
         </S.RegionBottomDiv>
 
         {authOk ? <S.MyPageAuthOkButton onClick={updateUserEditInfo}>저장</S.MyPageAuthOkButton> : <S.MyPageEditButton>저장</S.MyPageEditButton>}
